1392348d50353583a15de0320eccd8ea0ec272ae
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-07-03 21:45:04 +0300

    Insert/Erase

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -44,6 +44,48 @@ public:
 		//Говорим, что новый элемент является головой списка
 		Head = New;
 	}
+	void insert(int index, int Data) //Добавление элемента 
+	{
+		Element *New = new Element(Data);//Создаем новый элемент
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+		Temp = Temp->pNext;
+		Temp->pNext = New;
+	}
+	//     Removie Elements
+
+	void pop_front()
+	{
+		//запоминаем адрес удаляемого элемента
+		Element* Erased = Head;
+		//исключаем элемент из списка
+		Head = Head->pNext;
+		//удаляем элемент из памяти
+		delete Erased;
+	}
+	void pop_back()
+	{
+		Element*Temp = Head;
+		while (Temp->pNext->pNext)Temp = Temp->pNext;
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+		
+		/*if (Head->pNext == nullptr)return pop_front();
+		Element* Temp = Head;
+		while (Temp->pNext)Temp = Temp->pNext;
+		delete Temp->pNext;*/
+	}
+	void erase(int index)// Удаление элемента
+	{
+		//if (Head == nullptr)return pop_front();
+		Element* Temp = Head;
+		for (int i = 0; i < index-1; i++)
+			Temp = Temp->pNext;
+		Element* Erase = Temp->pNext;//запоминаем адрес удаляемого элемента
+		Temp->pNext = Temp->pNext->pNext;//исключаем элемент из списка
+		delete Erase; //удаляем элемент из памяти
+
+	}
 	////     Methods:
 	void print()const
 	{
@@ -54,18 +96,47 @@ public:
 			Temp = Temp->pNext;   //   Переход на следующий элемент
 		}	
 	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return push_front(Data);
+		Element* New = new Element(Data);//создаем новый элемент
+		//Дойти до конца списка
+		Element* Temp = Head;
+		while (Temp->pNext)Temp = Temp->pNext;
+		//присоеденяем новый элемент к списку
+		Temp->pNext = New;
+	}
+	
 };
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n;
+	
 	cout << "Введите размер списка: "; cin >> n;
 	ForwardList list;  //создали односвязный список под названием list
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		/*list.push_front(rand() % 100);*/
+		list.push_back(rand() % 100);
 	}
 	list.print();
+	list.push_back(123);
+	list.print();
+	list.pop_front();
+	list.print();
+	list.pop_back();
+	list.print();
 
+    int index;
+	int value;
+	cout << "Введите индекс: "; cin >> index;
+	cout << "Введите значение: "; cin >> value;
+	list.insert(index,value);
+	list.print();
+
+	cout << "Введите интекс: "; cin >> index;
+	list.erase(index);
+	list.print();
 }
