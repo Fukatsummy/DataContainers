ed5c9b83827667279a884dbf3486ca83221b2111
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-08-15 22:10:47 +0300

    Tree In Process

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -5,8 +5,87 @@ using std::endl;
 
 #define tab "\t"
 #define delimiter "\n---------------------------\n"
+#define Debug
+
+class Tree
+{
+	class Element
+	{
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :
+			Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+#ifdef DEBUG
+			cout << "EConstructor\t" << this << endl;
+#endif // DEBUG
+		}
+		~Element()
+		{
+#ifdef DEBUG		
+			cout << "EDestructor\t" << this << endl;
+#endif // DEBUG
+		}
+		friend class Tree;
+	}*Root;
+public:
+	Element* getRoot()const
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
+	{
+//#ifdef DEBUG	
+		cout << "TConstructor\t" << this << endl;
+//#endif // DEBUG
+	}
+	~Tree()
+	{
+//#ifdef DEBUG	
+		cout << "TDestructor\t" << this << endl;
+//#endif // DEBUG
+	}
+
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << tab;
+		print(Root->pRight);
+	}
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+	int n;
+	cout << "¬ведите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		int number = rand() % 100;
+		cout << number << tab;
+		tree.insert(rand()%100, tree.getRoot());
+	}
+	cout << endl;
+	tree.print(tree.getRoot());
+	cout << endl;
 }
